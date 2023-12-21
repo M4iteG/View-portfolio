7f8013c9a77ae9f7920e7392a180e4f9bb33de26
Maite Gijbels <Maite.Gijbels@student.pxl.be>
2023-12-21 16:52:16 +0100

    rqfs

---
+++ Index.js
@@ -0,0 +1,10 @@
+/*FOOTER*/
+let BTTButton = document.getElementById("BTT");
+function topFunction() {
+    document.body.scrollTop = 8;
+    document.documentElement.scrollTop = 8;
+}
+console.log(BTTButton)
+
+
+//raadpleging code: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
---
+++ README.md
@@ -0,0 +1,9 @@
+# wpl1-2324-start
+
+# Naam
+Maite Gijbels
+
+# Studenten-nummer
+12302004
+# Klas
+1DVOb
